3e50bc73331dda2368927e0592209d74b910cc78
MiddeTejaGirisha <mtejagirisha@gmail.com>
2024-06-22 03:44:31 +0530

    manytomany RM

--- Mapping/src/main/java/com/example/Mapping/Controller/StudentController.java
+++ Mapping/src/main/java/com/example/Mapping/Controller/StudentController.java
@@ -1,11 +1,8 @@
 package com.example.Mapping.Controller;
 
-import com.example.Mapping.Model.Course;
 import com.example.Mapping.Model.Student;
 import com.example.Mapping.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- Mapping/src/main/java/com/example/Mapping/MappingApplication.java
+++ Mapping/src/main/java/com/example/Mapping/MappingApplication.java
@@ -1,18 +1,9 @@
 package com.example.Mapping;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 
 @SpringBootApplication
-//@EnableJpaAuditing
-//@EnableCaching
 public class MappingApplication {
 
 	public static void main(String[] args) {
